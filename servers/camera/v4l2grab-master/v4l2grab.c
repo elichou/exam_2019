ebe707c225fb49771a53d293d6f9f027c3a5e1ec
Eli A. D <33091611+elichou@users.noreply.github.com>
2019-02-11 16:58:44 +0100

    salut toi (#14)
    * creation dossier servers/
    
    * ajout serveur camera
    
    * add IP adr for RPi3
    
    * remove garbage files
    
    * change in IP addr
    
    * changement adresse IP
    
    * Update v4l2grab.c
    
    * Update v4l2grab.c
    
    * Update v4l2grab.c

--- servers/camera/v4l2grab-master/v4l2grab.c
+++ servers/camera/v4l2grab-master/v4l2grab.c
@@ -872,7 +872,7 @@ static void usage(FILE* fp, int argc, char** argv)
 		"Options:\n"
 		"-d | --device name   Video device name [/dev/video0]\n"
 		"-h | --help          Print this message\n"
-		"-o | --output        JPEG outpur image is 'image'\n"
+		"-o | --output        JPEG output image is 'image'\n"
 		"-q | --quality       Set JPEG quality (0-100)\n"
 		"-m | --mmap          Use memory mapped buffers\n"
 		"-r | --read          Use read() calls\n"
@@ -880,7 +880,7 @@ static void usage(FILE* fp, int argc, char** argv)
 		"-W | --width         Set image width\n"
 		"-H | --height        Set image height\n"
 		"-I | --interval      Set frame interval (fps) (-1 to skip)\n"
-		"-c | --continuous    Do continous capture, stop with SIGINT.\n"
+		"-c | --continuous    Do continuous capture, stop with SIGINT.\n"
 		"-v | --version       Print version\n"
 		"",
 		argv[0]);
@@ -931,7 +931,7 @@ int main(int argc, char **argv)
 
 			case 'o':
 				// set jpeg filename
-				jpegFilename = "/var/image.jpeg";
+				jpegFilename = optarg;
 				break;
 
 			case 'q':
